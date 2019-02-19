a0305d385a4ef95d1d1aa759ef30c60ed90bd209
ilgonmic <ilgonmic@gmail.com>
2019-02-19 11:22:47 +0300

    Add 3d map

--- sources/master/Iterator.m
+++ sources/master/Iterator.m
@@ -3,7 +3,7 @@ function y = Iterator( x, plotData )
     xPlot = plotData(1, :);
     yPlot = plotData(2, :);
     for i = 1 : length(xPlot) - 1
-        if (xPlot(i) <= x && x < xPlot(i + 1))
+        if (xPlot(i) <= x && x <= xPlot(i + 1))
             k = (yPlot(i + 1) - yPlot(i))/(xPlot(i + 1) - xPlot(i));
             b = yPlot(i) - k*xPlot(i);
             y = k*x + b;
@@ -11,6 +11,7 @@ function y = Iterator( x, plotData )
         end
     end
     
+    disp('YD')
     y = 0;
 
 end
--- sources/master/lamereyCoding.m
+++ sources/master/lamereyCoding.m
@@ -10,7 +10,8 @@ alphabetSize = 32;
 
 alpha = 0.001;
 
-data2 = ['мурка'; 'котик'];
+data = ['мурка'];
+data2 = ['норка'];
 
 x = 0 : 0.1 : 1;
 y = x;
@@ -21,7 +22,7 @@ plot(x,y,'r');
 hold on;
 grid
 
-plotData = Calculator(data2, alphabetSize);
+plotData = Calculator(data, alphabetSize);
 
 plot(plotData(1,:), plotData(2,:), 'k')
 
@@ -43,3 +44,39 @@ xLamerey=xLamerey(:);
 yLamerey=[xLamerey(2:end); xLamerey(end)];
 
 plot(xLamerey, yLamerey, 'g');
+
+% ========
+
+plotData2 = Calculator(data2, alphabetSize);
+
+plot(plotData2(1,:), plotData2(2,:), 'k')
+
+x1 = 0.0122;
+
+for r = 1:nTransient
+    x1 = Iterator(x1, plotData);
+end
+
+X(1) = x1;
+for r = 2:nCalc
+    X(r) = Iterator(X(r-1), plotData);
+end
+
+disp(char(X*alphabetSize+double('а')));
+
+xLamerey2=[X; X];
+xLamerey2=xLamerey2(:);
+yLamerey2=[xLamerey2(2:end); xLamerey2(end)];
+
+plot(xLamerey2, yLamerey2, 'g');
+
+figure
+[Xp,Yp] = meshgrid(plotData(1, :), plotData2(1, :));
+
+for q = 1:length(Xp)
+    for w = 1:length(Yp)
+        Z(q, w) = plotData(2, q) + plotData2(2, w);
+    end
+end
+
+surf(Xp, Yp, Z)
